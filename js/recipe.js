fc731a68460a9afc6c0b4c5ef88522ff50842220
Stephen <sagelvl40@gmail.com>
2023-10-09 22:47:22 +0100

    redo JavaScript for Recipe pa page add lists and pictures dynamically

--- js/recipe.js
+++ js/recipe.js
@@ -1,17 +1,17 @@
 // Create Recipe Object
 // This will list 5 ingredients, the name and an image
 
-function Recipe(name, items, image) {
+function Recipe(name, items, imageUrl) {
   this.name = name;
   this.items = items;
-  this.image = image;
+  this.imageURL = imageUrl;
 }
 
 const recipeCards = [
   new Recipe(
     "Chicken Quesadillas",
     ["tortillas, chicken breast, cheddar cheese, salsa, sour cream"],
-    "https://placehold.co/250"
+    "./PantryPet/pantry/images/Chicken-Quesadillas-7.jpg"
   ),
   new Recipe(
     "Pesto Chicken",
@@ -40,7 +40,7 @@ const recipeCards = [
   ),
   new Recipe(
     "Beef Stir Fry",
-    ["beef strips, teriyaki sauce, noodles, peppers, broccoli"],
+    ["beef strips, teriyaki sauce, rice, peppers, broccoli"],
     "https://placehold.co/250"
   ),
   new Recipe(
@@ -50,17 +50,43 @@ const recipeCards = [
   ),
   new Recipe(
     "Beef and Bacon Burger",
-    ["burgers, lettuce, cheddar cheese, bacon, bbq sauce"],
+    ["burgers, lettuce, cheddar cheese, bacon, red onion"],
     "https://placehold.co/250"
   ),
 ];
+// Function to populate the recipe cards from the object
+function populateRecipeCards() {
+  const recipeList = document.getElementById("recipeList");
 
-console.log(recipeCards);
+  recipeCards.forEach((recipe) => {
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipe");
+
+    // Display the recipe image, name and list - images do not show on the page these should be showing dynamiclly
+    const image = document.createElement("img");
+    image.src = recipe.imageUrl;
+    image.alt = recipe.name;
+    recipeDiv.appendChild(image);
+
+    const recipeName = document.createElement("h3");
+    recipeName.textContent = recipe.name;
+    recipeDiv.appendChild(recipeName);
+
+    const ingredientsList = document.createElement("ul");
+    recipe.items.forEach((item) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = item;
+      ingredientsList.appendChild(listItem);
+    });
+    recipeDiv.appendChild(ingredientsList);
+
+    recipeList.appendChild(recipeDiv);
+  });
+}
+
+// Call the function to populate recipe cards
+populateRecipeCards();
 
 // store the items in local storage
 
 localStorage.setItem("recipes", JSON.stringify(recipeCards));
-
-// Display Recipe as a list for now
-
-// Toggle the items on and off based on the user data stored.
