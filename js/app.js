9a8be4a6bb862e1ad0e739f61b8312bf80b3c59f
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-10-11 14:30:41 +0100

    Add remove button

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const ingredientsForm = document.getElementById("ingredientsForm");
 const ingredientName = document.getElementById("ingredientName");
 const recipeForm = document.getElementById("recipeForm");
 const recipeIngredients = document.getElementById("recipeIngredients");
+//const removeIngredient = document.getElementById("removeIngredient");
 let yourPantry = document.getElementById("yourPantry");
 
 let pantryList = [];
@@ -12,6 +13,8 @@ let pantryList = [];
 // render list function, call outside
 
 function populateList() {
+  // clear the ul
+  yourPantry.textContent = "";
   if (localStorage.getItem("ingredients")) {
     pantryList = JSON.parse(localStorage.getItem("ingredients"));
 
@@ -39,13 +42,38 @@ function addIngredient(event) {
 
 ingredientsForm.addEventListener("submit", addIngredient);
 
+//user input
+//click the button
+//loop around array in local storage
+//if match, remove
+
+function removeItem(event) {
+  event.preventDefault();
+  let removedIngredient = event.target.removedIngredient.value;
+
+  for (let i = 0; i < pantryList.length; i++) {
+    if (removedIngredient === pantryList[i]) {
+      pantryList.splice(i, 1);
+    }
+  }
+  // set ls to be pantry list
+  localStorage.setItem("ingredients", JSON.stringify(pantryList));
+  populateList();
+}
+//
+//
+
+//
+// }
+removeIngredient.addEventListener("submit", removeItem);
+
 function addRecipeIngredient(event) {
   event.preventDefault();
   let addedIngredient = event.target.recipeIngredients.value;
   pantryList.push(addedIngredient);
 
   localStorage.setItem("ingredients", JSON.stringify(pantryList));
-  const ul = document.getElementById("ul");
+
   const li = document.createElement("li");
   li.textContent = addedIngredient;
   yourPantry.appendChild(li);
